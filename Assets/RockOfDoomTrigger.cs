4ac553131d3469b54afba4baffa3d6dee63e6915
phil.n.weber@gmail.com <phil.n.weber@gmail.com@39f3aa7d-56e5-2ea2-d00d-e0713464e15b>
2015-02-24 02:49:25 +0000

    Added a trigger event around the rock, now known as the rock of doom :)

---
+++ Assets/RockOfDoomTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockOfDoomTrigger : MonoBehaviour {
+
+	public GameObject enemy;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.name == "Cowboy")
+		{
+			GameObject[] spawns = GameObject.FindGameObjectsWithTag ("enemyspawn");
+			foreach(var spawn in spawns)
+			{
+				Instantiate (enemy, spawn.transform.position, spawn.transform.rotation);
+			}
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		Debug.Log ("Object Stayed in Collider");
+	}
+
+	void OnTriggerExit2D(Collider2D other ) {
+		Debug.Log ("Object Left Collider");
+	}
+
+}
