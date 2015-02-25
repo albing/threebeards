d2aa912d9f4a366ef19c05cece03fbd45221e90d
sirgregsalot@gmail.com <sirgregsalot@gmail.com@39f3aa7d-56e5-2ea2-d00d-e0713464e15b>
2015-02-25 03:32:24 +0000

    added files I forgot in the last check-in and started a scene selector (which should be ignored for the time being because I blow at SVN)

---
+++ Assets/scripts/AreaTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AreaTrigger : MonoBehaviour {
+
+	public string text;
+	public GameObject messageWindowObject;
+
+	private GameObject message;
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if(other.CompareTag("Player"))
+		{
+			var canvas = GameObject.Find ("Message Canvas");
+			if(!canvas)
+			{
+				var c = Instantiate(new Canvas());
+				c.name = "Message Canvas";
+			}
+			
+			message = (GameObject) Instantiate(messageWindowObject);
+
+			var pum = message.GetComponent<PopUpMessage>();
+			pum.destroyType = PopUpMessage.MessageDestroyType.Proximity;
+
+			if(!string.IsNullOrEmpty(text))
+				pum.text = this.text;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.CompareTag("Player"))
+			Destroy (message);
+	}
+
+}
