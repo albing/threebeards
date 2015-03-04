34dc36a17101be42f125f4cca2e3bb1746c4cbd7
ronnyboy57@gmail.com <ronnyboy57@gmail.com@39f3aa7d-56e5-2ea2-d00d-e0713464e15b>
2015-03-04 03:30:28 +0000

    Tried restoring the files that greg seems to have deleted.

---
+++ Assets/scripts/LevelChangeScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChangeScript : MonoBehaviour {
+
+	public void tutorialLevel()
+	{
+		Application.LoadLevel ("Tutorial Level");
+	}
+}
