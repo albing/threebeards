cad4ad7bf8a66c8bbb9708fc4812e91cf564a116
sirgregsalot@gmail.com <sirgregsalot@gmail.com@39f3aa7d-56e5-2ea2-d00d-e0713464e15b>
2015-03-16 04:55:47 +0000

    oops, forgot the crucial files.

---
+++ Assets/TutorialLevelEndCondition.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialLevelEndCondition : MonoBehaviour {
+
+	// have to make this one public because it starts out as inactive
+	public GameObject victoryPanel;
+
+	private GameObject messageCanvas, scoreText;
+	private bool halted = false;
+
+	void Start() {
+		messageCanvas = GameObject.Find ("Message Canvas");
+		scoreText = GameObject.Find ("Score Text");
+		unhalt ();
+	}
+	
+	// TODO: OnPauseGame event: 
+	// http://answers.unity3d.com/questions/7544/how-do-i-pause-my-game.html
+	void Update() {
+		if(Shoot.score >= 20 && !halted)
+		{
+			halted = !halted;
+			halt ();
+		}
+	}
+
+	private void halt()
+	{
+		Time.timeScale = 0;
+		
+		if(victoryPanel)
+			victoryPanel.SetActive(true);
+		
+		messageCanvas.SetActive(false);
+		scoreText.SetActive(false);
+	}
+
+	private void unhalt()
+	{
+		Time.timeScale = 1.0f;
+	}
+}
---
+++ Assets/scripts/LevelChangeScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChangeScript : MonoBehaviour {
+
+	public void tutorialLevel()
+	{
+		Shoot.score = 0;
+		Application.LoadLevel ("Tutorial Level");
+	}
+
+	public void levelSelectScreen()
+	{
+		Application.LoadLevel ("Level Select");
+	}
+}
