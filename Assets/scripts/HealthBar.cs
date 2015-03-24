fa6f8352646d6aa1d0d96816cbf44446b607e0ef
sirgregsalot@gmail.com <sirgregsalot@gmail.com@39f3aa7d-56e5-2ea2-d00d-e0713464e15b>
2015-03-24 17:12:14 +0000

    added health bar UI element in tutorial level

---
+++ Assets/scripts/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/*
+ * N.B.: The object this is attached to should be x-scaled at 1
+ * 
+ */
+
+
+public class HealthBar : MonoBehaviour {
+
+	Vector3 scale;
+
+	void Start()
+	{
+		scale = transform.lossyScale;
+	}
+
+	void setHealth(float health)
+	{
+		scale.x = health / 100;
+		transform.localScale = scale;
+	}
+}
