2201bf9b256e01e7abb0bb87efb7b93dd5419c0f
sirgregsalot@gmail.com <sirgregsalot@gmail.com@39f3aa7d-56e5-2ea2-d00d-e0713464e15b>
2015-04-01 03:08:28 +0000

    added enemy death noises

---
+++ Assets/DeathNoises.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathNoises : MonoBehaviour {
+
+	public AudioClip[] deathSounds;
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if(other.CompareTag("player projectile"))
+			AudioSource.PlayClipAtPoint(deathSounds[Random.Range(0, deathSounds.Length)], transform.position);
+	}
+}
