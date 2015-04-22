45f6f0073b7839516457c2312ce847a1abf071d1
sirgregsalot@gmail.com <sirgregsalot@gmail.com@39f3aa7d-56e5-2ea2-d00d-e0713464e15b>
2015-04-22 05:30:05 +0000

    I'm a dork and forgot to include files

---
+++ Assets/scripts/Level1IntroCutscene.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1IntroCutscene : MonoBehaviour {
+	
+	public GameObject playableCowboy, beavers;
+	public Canvas canvas;
+	public LevelOneEndCondition endCondition;
+
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.X))
+		{
+			EndCutscene();
+		}
+	}
+
+	void Start () {
+		beavers.SetActive (false);
+		canvas.gameObject.SetActive (false);
+		endCondition.enabled = false;
+	}
+
+	public void EndCutscene()
+	{
+		playableCowboy.SetActive (true);
+		this.gameObject.SetActive (false);
+		beavers.SetActive (true);
+		canvas.gameObject.SetActive (true);
+		endCondition.enabled = true;
+	}
+}
