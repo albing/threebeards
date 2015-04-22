7047052a5e4303a7322b4b31954024b7bde81963
sirgregsalot@gmail.com <sirgregsalot@gmail.com@39f3aa7d-56e5-2ea2-d00d-e0713464e15b>
2015-04-22 14:56:54 +0000

    spider palin now says things

---
+++ Assets/scripts/LivingNoises.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LivingNoises : MonoBehaviour {
+
+	public AudioClip[] clips;
+	public float delayInSeconds;
+
+	private AudioSource source;
+	private float time = 0;
+
+	void Start() {
+		source = GetComponent<AudioSource> ();
+	}
+
+	void Update () {
+		time += Time.deltaTime;
+
+		if(time > delayInSeconds)
+		{
+			time = 0;
+			source.PlayOneShot(clips[Random.Range(0, clips.Length)]);
+		}
+	}
+}
