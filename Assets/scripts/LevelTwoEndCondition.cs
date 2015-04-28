2ae108dc39bcf8b0e1f44e2fed01c4c1cdcbc552
ronnyboy57@gmail.com <ronnyboy57@gmail.com@39f3aa7d-56e5-2ea2-d00d-e0713464e15b>
2015-04-28 23:31:52 +0000

    Noticed that level 2 didn't have the health & canvas working so i fixed that.

---
+++ Assets/scripts/LevelTwoEndCondition.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+
+public class LevelTwoEndCondition : MonoBehaviour {
+	
+	// have to make this one public because it starts out as inactive
+	public GameObject victoryPanel;
+	public GameObject deadPanel;
+	public HealthManager playerHealth;
+	public GameObject playerHealthBar;
+	
+	private GameObject scoreText;
+	private bool halted = false;
+	private bool dead = false;
+	private bool beatLevel = false;
+	
+	void Start() {
+		scoreText = GameObject.Find ("Score Text");
+		unhalt ();
+	}
+	
+	// TODO: OnPauseGame event: 
+	// http://answers.unity3d.com/questions/7544/how-do-i-pause-my-game.html
+	void Update() {
+
+		if (playerHealth.health <= 0 && !halted) {
+			halted = !halted;
+			dead = true;
+			halt ();
+		}
+	}
+	
+	private void halt()
+	{
+		Time.timeScale = 0;
+		
+		if(beatLevel)
+			victoryPanel.SetActive(true);
+		
+		if (dead)
+			deadPanel.SetActive (true);
+		
+		playerHealthBar.SetActive (false);
+		scoreText.SetActive(false);
+	}
+	
+	public void unhalt()
+	{
+		Time.timeScale = 1.0f;;
+	}
+}
